3a7b0b4022a13fb97a255f4e44cc5000f784b5c2
unknown <charlierossouw@outlook.com>
2021-02-11 14:23:40 -0800

    feat: Shopping cart page index and routing

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -5,6 +5,7 @@ import "./styles/main.scss"
 
 import { HomePageIndex } from "./components/HomePage/HomePageIndex"
 import { FavouritesPageIndex } from './components/FavouritesPage/FavouritesPageIndex'
+import { ShoppingCartPageIndex } from './components/ShoppingCartPage/ShoppingCartPageIndex'
 
 function App() {
   return (
@@ -12,6 +13,7 @@ function App() {
         <Switch>
           <Route exact path="/" component={HomePageIndex} />
           <Route exact path="/favourites" component={FavouritesPageIndex} />      
+          <Route exact path="/shoppingcart" component={ShoppingCartPageIndex} />      
         </Switch>
     </BrowserRouter>
   );
---
+++ client/src/components/ShoppingCartPage/ShoppingCartPageIndex.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+interface Props {
+
+}
+
+export const ShoppingCartPageIndex: React.FC<Props> = () => {
+    return (
+        <h1>Shopping cart page wooo</h1>
+    );
+}
