d9f39239b17daa92c3eb1fce6fd135b2b2796d88
Charles <47355793+MeatBoyed@users.noreply.github.com>
2021-02-11 23:06:25 +0200

    feat: Routing operational

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,13 +1,19 @@
 import React from "react"
+import { BrowserRouter, Route, Switch } from 'react-router-dom'
+
 import "./styles/main.scss"
 
 import { HomePageIndex } from "./components/HomePage/HomePageIndex"
+import { FavouritesPageIndex } from './components/FavouritesPage/FavouritesPageIndex'
 
 function App() {
   return (
-    <React.Fragment>
-      <HomePageIndex />
-    </React.Fragment>
+    <BrowserRouter>
+        <Switch>
+          <Route exact path="/" component={HomePageIndex} />
+          <Route exact path="/favourites" component={FavouritesPageIndex} />      
+        </Switch>
+    </BrowserRouter>
   );
 }
 
---
+++ client/src/components/FavouritesPage/FavouritesPageIndex.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+interface Props {
+
+}
+
+export const FavouritesPageIndex: React.FC<Props> = () => {
+    return (
+        <h1>Favourites Page wooo</h1>
+    );
+}
