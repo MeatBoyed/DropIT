567b606455eee1349ae232f8ee89702478e59ad5
Charles <47355793+MeatBoyed@users.noreply.github.com>
2021-02-13 12:08:41 +0200

    feat: Favourites title more friendly

--- client/src/components/FavouritesPage/FavouritesSection.tsx
+++ client/src/components/FavouritesPage/FavouritesSection.tsx
@@ -1,32 +1,30 @@
-import React from 'react'
+import React from 'react';
 
 // Import Components
-import { ItemCard } from '../ItemCard'
+import { ItemCard } from '../ItemCard';
 
-// Import Thumbnails (temp) 
-import thumbnailIcon from "../../images/thumbnail5.png"
-import thumbnailIcon1 from "../../images/thumbnail3.png"
-import thumbnailIcon2 from "../../images/thumbnail2.png"
-import thumbnailIcon3 from "../../images/thumbnail4.png"
-import thumbnailIcon4 from "../../images/thumbnail1.png"
+// Import Thumbnails (temp)
+import thumbnailIcon from '../../images/thumbnail5.png';
+import thumbnailIcon1 from '../../images/thumbnail3.png';
+import thumbnailIcon2 from '../../images/thumbnail2.png';
+import thumbnailIcon3 from '../../images/thumbnail4.png';
+import thumbnailIcon4 from '../../images/thumbnail1.png';
 
-interface Props {
-
-}
+interface Props {}
 
 export const FavouritesSection: React.FC<Props> = () => {
-    return (
-        <section id="FavouritesSection">
-            <h3 className="sectionTitle">Favourites</h3>
-            <div className="favouritesContainer">
-                <ItemCard thumbnailIcon={thumbnailIcon} />
-                <ItemCard thumbnailIcon={thumbnailIcon1} />
-                <ItemCard thumbnailIcon={thumbnailIcon2} />
-                <ItemCard thumbnailIcon={thumbnailIcon3} />
-                <ItemCard thumbnailIcon={thumbnailIcon1} />
-                <ItemCard thumbnailIcon={thumbnailIcon4} />
-                <ItemCard thumbnailIcon={thumbnailIcon} />
-            </div>
-        </section>
-    );
-}
+  return (
+    <section id="FavouritesSection">
+      <h3 className="sectionTitle">Your Favourites</h3>
+      <div className="favouritesContainer">
+        <ItemCard thumbnailIcon={thumbnailIcon} />
+        <ItemCard thumbnailIcon={thumbnailIcon1} />
+        <ItemCard thumbnailIcon={thumbnailIcon2} />
+        <ItemCard thumbnailIcon={thumbnailIcon3} />
+        <ItemCard thumbnailIcon={thumbnailIcon1} />
+        <ItemCard thumbnailIcon={thumbnailIcon4} />
+        <ItemCard thumbnailIcon={thumbnailIcon} />
+      </div>
+    </section>
+  );
+};
