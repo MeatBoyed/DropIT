18cc1eb9b7cb15e2e22d3238e304966ec387d1ab
Charles <47355793+MeatBoyed@users.noreply.github.com>
2021-02-13 02:46:03 +0200

    fix: Removal of SVG files

--- client/src/components/ShoppingCartPage/CartItem.tsx
+++ client/src/components/ShoppingCartPage/CartItem.tsx
@@ -1,8 +1,5 @@
 import React from 'react'
 
-import FavouritesIcon from '../../images/favourite.svg'
-import RemoveIcon from '../../images/remove.svg'
-
 interface Props {
     thumbnail: string,
     colour: string
@@ -11,7 +8,7 @@ interface Props {
 export const CartItem: React.FC<Props> = ({ thumbnail, colour }) => {
     return (
         <div className="CartItem">
-            <img src={FavouritesIcon} alt="" className="favouriteIcon" />
+            <svg className="favouriteIcon" width="17" height="17" viewBox="0 0 17 17"><g transform="translate(-150.604 -262.604)"><circle cx="8.5" cy="8.5" r="8.5" transform="translate(150.604 262.604)" fill="#b8b8b8"/><g transform="translate(154.355 267.391)"><g transform="translate(0 0)"><path d="M4.558,39.265C1.883,36.925,0,35.685,0,33.713A2.585,2.585,0,0,1,2.519,31a2.508,2.508,0,0,1,2.222,1.5A2.508,2.508,0,0,1,6.963,31a2.585,2.585,0,0,1,2.519,2.713c0,1.971-1.881,3.209-4.558,5.551A.278.278,0,0,1,4.558,39.265Z" transform="translate(0 -31)" fill="#fff"/></g></g></g></svg>
             <img src={thumbnail} alt="" className="thumbnail"/>
             <div className="cartItemInfo">
                 <p className="itemTitle">Beats Solo 3</p>
@@ -37,7 +34,7 @@ export const CartItem: React.FC<Props> = ({ thumbnail, colour }) => {
                     </svg>
                 </div>
             </div>
-            <img src={RemoveIcon} alt="" className="removeIcon" />
+            <svg className="removeIcon" width="23.257" height="23.257" viewBox="0 0 23.257 23.257"><path d="M11.628,0A11.628,11.628,0,1,0,23.257,11.628,11.642,11.642,0,0,0,11.628,0Zm5.814,13.082H5.814V10.175H17.442Z"/></svg>
         </div>
     );
 }
