0d14a64e7d5dacfe7db755c48a7dc11d7bf337c2
Charles <47355793+MeatBoyed@users.noreply.github.com>
2021-02-17 16:09:56 +0200

    feat: Item card links to Item Page

--- client/src/components/HomePage/ShoppingSection.tsx
+++ client/src/components/HomePage/ShoppingSection.tsx
@@ -1,88 +1,91 @@
-import React, { useState, useEffect } from "react";
-import { firestore } from "../../firebase";
+import React, { useState, useEffect } from 'react';
+import { firestore } from '../../firebase';
 
 // Import Components
-import { StoreCard } from "./StoreCard";
-import { ItemCard } from "../ItemCard";
+import { StoreCard } from './StoreCard';
+import { ItemCard } from '../ItemCard';
 
 interface Vendors {
-	id: string;
-	logo: string;
+  id: string;
+  logo: string;
 }
 
 interface Items {
-	id: string;
-	title: string;
-	price: number;
-	mainImage: string;
+  id: string;
+  vendor: string;
+  title: string;
+  price: number;
+  mainImage: string;
 }
 
 export const ShoppingSection: React.FC = () => {
-	// Get Item data irrispective of the vendor
-	// Iterate over data to render components
+  // Get Item data irrispective of the vendor
+  // Iterate over data to render components
 
-	const [vendors, setVendors] = useState<Vendors[]>([]);
-	const [items, setItems] = useState<Items[]>([]);
+  const [vendors, setVendors] = useState<Vendors[]>([]);
+  const [items, setItems] = useState<Items[]>([]);
 
-	// Get all vendor icons, don't have any vendor selected (highlighted like they are)
-	const GetVendors = async () => {
-		const vendors = await firestore.collection("Vendors").get();
-		vendors.docs.forEach((doc) => {
-			setVendors((vendors) => [
-				...vendors,
-				{
-					id: doc.id,
-					logo: doc.data().logoImage,
-				},
-			]);
-		});
-	};
+  // Get all vendor icons, don't have any vendor selected (highlighted like they are)
+  const GetVendors = async () => {
+    const vendors = await firestore.collection('Vendors').get();
+    vendors.docs.forEach((doc) => {
+      setVendors((vendors) => [
+        ...vendors,
+        {
+          id: doc.id,
+          logo: doc.data().logoImage,
+        },
+      ]);
+    });
+  };
 
-	const GetItems = async () => {
-		const items = await firestore.collection("Items").get();
-		items.docs.forEach((doc) => {
-			setItems((items) => [
-				...items,
-				{
-					id: doc.id,
-					title: doc.data().title,
-					price: doc.data().price,
-					mainImage: doc.data().images.mainImage,
-				},
-			]);
-		});
-	};
+  const GetItems = async () => {
+    const items = await firestore.collection('Items').get();
+    items.docs.forEach((doc) => {
+      setItems((items) => [
+        ...items,
+        {
+          id: doc.id,
+          vendor: doc.data().vendor,
+          title: doc.data().title,
+          price: doc.data().price,
+          mainImage: doc.data().images.mainImage,
+        },
+      ]);
+    });
+  };
 
-	useEffect(() => {
-		GetVendors();
-		GetItems();
-	}, []);
+  useEffect(() => {
+    GetVendors();
+    GetItems();
+  }, []);
 
-	console.log(items);
+  console.log(items);
 
-	return (
-		<section id="ShoppingSection">
-			<div id="SelectStoresContainer">
-				<h3 className="sectionTitle">Choose Store</h3>
-				<div className="storeSelectContainer">
-					{vendors.map((vendor) => (
-						<StoreCard key={vendor.id} vendorLogo={vendor.logo} />
-					))}
-				</div>
-			</div>
-			<div id="StoreItemsContainer">
-				<h3 className="sectionTitle">Beats Products</h3>
-				<div className="itemsContainer">
-					{items.map((item) => (
-						<ItemCard
-							key={item.id}
-							title={item.title}
-							price={item.price}
-							mainImage={item.mainImage}
-						/>
-					))}
-				</div>
-			</div>
-		</section>
-	);
+  return (
+    <section id="ShoppingSection">
+      <div id="SelectStoresContainer">
+        <h3 className="sectionTitle">Choose Store</h3>
+        <div className="storeSelectContainer">
+          {vendors.map((vendor) => (
+            <StoreCard key={vendor.id} vendorLogo={vendor.logo} />
+          ))}
+        </div>
+      </div>
+      <div id="StoreItemsContainer">
+        <h3 className="sectionTitle">Beats Products</h3>
+        <div className="itemsContainer">
+          {items.map((item) => (
+            <ItemCard
+              key={item.id}
+              url={`/${item.vendor}/${item.id}`}
+              title={item.title}
+              price={item.price}
+              mainImage={item.mainImage}
+            />
+          ))}
+        </div>
+      </div>
+    </section>
+  );
 };
