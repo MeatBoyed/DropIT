cbff297d21335a0f5c4735fc73888e2fbb2db1a9
Charles <47355793+MeatBoyed@users.noreply.github.com>
2021-02-20 23:43:08 +0200

    fix: Removed all favourites section

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,51 +1,37 @@
-import React from "react";
-import { BrowserRouter, Route, Switch } from "react-router-dom";
+import React from 'react';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
 
-import "./styles/main.scss";
+import './styles/main.scss';
 
-import { ShoppingCartContextProvider } from "./components/ShoppingCartContext";
-import { LoadingSpinner } from "./components/LoadingSpinner";
-const HomePageIndex = React.lazy(
-	() => import("./components/HomePage/HomePageIndex")
-);
-const FavouritesPageIndex = React.lazy(
-	() => import("./components/FavouritesPage/FavouritesPageIndex")
-);
-const ShoppingCartPageIndex = React.lazy(
-	() => import("./components/ShoppingCartPage/ShoppingCartPageIndex")
-);
-const ItemPageIndex = React.lazy(
-	() => import("./components/ItemPage/ItemPageIndex")
-);
+import { ShoppingCartContextProvider } from './components/ShoppingCartContext';
+import { LoadingSpinner } from './components/LoadingSpinner';
+const HomePageIndex = React.lazy(() => import('./components/HomePage/HomePageIndex'));
+const ShoppingCartPageIndex = React.lazy(() => import('./components/ShoppingCartPage/ShoppingCartPageIndex'));
+const ItemPageIndex = React.lazy(() => import('./components/ItemPage/ItemPageIndex'));
 
 const PageNotFound = () => (
-	<div className="PagenotFound">
-		<h1>Sorry but the page is no there</h1>
-		<h3>Error 404</h3>
-	</div>
+  <div className="PagenotFound">
+    <h1>Sorry but the page is no there</h1>
+    <h3>Error 404</h3>
+  </div>
 );
 
 function App() {
-	return (
-		<BrowserRouter>
-			<React.Suspense fallback={<LoadingSpinner />}>
-				<Switch>
-					<ShoppingCartContextProvider>
-						<Route exact path="/" component={HomePageIndex} />
-						<Route exact path="/favourites" component={FavouritesPageIndex} />
-						<Route
-							exact
-							path="/shoppingcart"
-							component={ShoppingCartPageIndex}
-						/>
-						<Route exact path="/:storename/:itemid" component={ItemPageIndex} />
-						<Route exact path="/404" component={PageNotFound} />
-						{/* <Route path="" component={PageNotFound} /> */}
-					</ShoppingCartContextProvider>
-				</Switch>
-			</React.Suspense>
-		</BrowserRouter>
-	);
+  return (
+    <BrowserRouter>
+      <React.Suspense fallback={<LoadingSpinner />}>
+        <Switch>
+          <ShoppingCartContextProvider>
+            <Route exact path="/" component={HomePageIndex} />
+            <Route exact path="/shoppingcart" component={ShoppingCartPageIndex} />
+            <Route exact path="/:storename/:itemid" component={ItemPageIndex} />
+            <Route exact path="/404" component={PageNotFound} />
+            {/* <Route path="" component={PageNotFound} /> */}
+          </ShoppingCartContextProvider>
+        </Switch>
+      </React.Suspense>
+    </BrowserRouter>
+  );
 }
 
 export default App;
