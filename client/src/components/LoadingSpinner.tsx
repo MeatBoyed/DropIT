37ae8bb08fe995746989cd20ee71c0dcd63c7f60
Charles <47355793+MeatBoyed@users.noreply.github.com>
2021-02-20 23:37:20 +0200

    fix: Removed Select Store section

---
+++ client/src/components/LoadingSpinner.tsx
@@ -0,0 +1,20 @@
+import React from 'react';
+
+export const LoadingSpinner: React.FC = () => {
+  return (
+    <div className="lds-spinner">
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+    </div>
+  );
+};
