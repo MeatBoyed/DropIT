7919bcc2a5f6d7afa5cc5f0cb855416be323bf2a
Charles <47355793+MeatBoyed@users.noreply.github.com>
2021-02-22 01:57:47 +0200

    bug: Lazy load 404 page

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,65 +1,45 @@
-import React from "react";
-import { BrowserRouter, Route, Switch } from "react-router-dom";
+import React from 'react';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
 
-import "./styles/main.scss";
+import './styles/main.scss';
 
-import { ShoppingCartContextProvider } from "./components/ShoppingCartContext";
-import { LoadingSpinner } from "./components/LoadingSpinner";
-import { Navbar } from "./components/Navbar";
-import { SideBar } from "./components/SideBar";
-const HomePageIndex = React.lazy(
-	() => import("./components/HomePage/HomePageIndex")
-);
-const ShoppingCartPageIndex = React.lazy(
-	() => import("./components/ShoppingCartPage/ShoppingCartPageIndex")
-);
-const ItemPageIndex = React.lazy(
-	() => import("./components/ItemPage/ItemPageIndex")
-);
-
-const PageNotFound = () => (
-	<div className="PagenotFound">
-		<h1>Sorry but the page is no there</h1>
-		<h3>Error 404</h3>
-	</div>
-);
+import { ShoppingCartContextProvider } from './components/ShoppingCartContext';
+import { LoadingSpinner } from './components/LoadingSpinner';
+import { Navbar } from './components/Navbar';
+import { SideBar } from './components/SideBar';
+const HomePageIndex = React.lazy(() => import('./components/HomePage/HomePageIndex'));
+const ShoppingCartPageIndex = React.lazy(() => import('./components/ShoppingCartPage/ShoppingCartPageIndex'));
+const ItemPageIndex = React.lazy(() => import('./components/ItemPage/ItemPageIndex'));
+const PageNotFound = React.lazy(() => import('./components/PageNotFound'));
 
 function App() {
-	return (
-		<BrowserRouter>
-			<React.Suspense
-				fallback={
-					<div className="spinnerWrapper">
-						<LoadingSpinner />
-					</div>
-				}
-			>
-				<section id="MainSection">
-					<Navbar />
-					<div id="PageContainer">
-						<SideBar />
-						<Switch>
-							<ShoppingCartContextProvider>
-								<Route exact path="/" component={HomePageIndex} />
-								<Route
-									exact
-									path="/shoppingcart"
-									component={ShoppingCartPageIndex}
-								/>
-								<Route
-									exact
-									path="/:storename/:itemid"
-									component={ItemPageIndex}
-								/>
-								<Route exact path="/404" component={PageNotFound} />
-								{/* <Route path="" component={PageNotFound} /> */}
-							</ShoppingCartContextProvider>
-						</Switch>
-					</div>
-				</section>
-			</React.Suspense>
-		</BrowserRouter>
-	);
+  return (
+    <BrowserRouter>
+      <React.Suspense
+        fallback={
+          <div className="spinnerWrapper">
+            <LoadingSpinner />
+          </div>
+        }
+      >
+        <section id="MainSection">
+          <Navbar />
+          <div id="PageContainer">
+            <SideBar />
+            <Switch>
+              <ShoppingCartContextProvider>
+                <Route exact path="/" component={HomePageIndex} />
+                <Route exact path="/shoppingcart" component={ShoppingCartPageIndex} />
+                <Route exact path="/:storename/:itemid" component={ItemPageIndex} />
+                <Route exact path="/404" component={PageNotFound} />
+                {/* <Route path="" component={PageNotFound} /> */}
+              </ShoppingCartContextProvider>
+            </Switch>
+          </div>
+        </section>
+      </React.Suspense>
+    </BrowserRouter>
+  );
 }
 
 export default App;
