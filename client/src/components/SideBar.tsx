046f91a99a8b7cca3f0c278314d1ef55bd82bed9
Charles <47355793+MeatBoyed@users.noreply.github.com>
2021-02-22 00:39:58 +0200

    fix: Nested <a> in side bar

--- client/src/components/SideBar.tsx
+++ client/src/components/SideBar.tsx
@@ -11,34 +11,26 @@ export const SideBar: React.FC = () => {
             Clothes
           </Link>
         </li>
-        <Link to="">
-          <li className="link">
-            <Link className="tag" to="">
-              Books
-            </Link>
-          </li>
-        </Link>
-        <Link to="">
-          <li className="link">
-            <Link className="tag" to="">
-              House Appliances
-            </Link>
-          </li>
-        </Link>
-        <Link to="">
-          <li className="link">
-            <Link className="tag" to="">
-              Tech
-            </Link>
-          </li>
-        </Link>
-        <Link to="">
-          <li className="link">
-            <Link className="tag" to="">
-              About
-            </Link>
-          </li>
-        </Link>
+        <li className="link">
+          <Link className="tag" to="">
+            Books
+          </Link>
+        </li>
+        <li className="link">
+          <Link className="tag" to="">
+            House Appliances
+          </Link>
+        </li>
+        <li className="link">
+          <Link className="tag" to="">
+            Tech
+          </Link>
+        </li>
+        <li className="link">
+          <Link className="tag" to="">
+            About
+          </Link>
+        </li>
       </ul>
     </section>
   );
