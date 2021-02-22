e0c0f120e5babf760c688ceff54907418c576f89
Charles <47355793+MeatBoyed@users.noreply.github.com>
2021-02-22 02:55:50 +0200

    bug: Optimization changes

---
+++ client/src/components/PageNotFound.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+export const PageNotFound: React.FC = () => {
+  return (
+    <div className="PagenotFound">
+      <h1>Sorry but the page is no there</h1>
+      <h3>Error 404</h3>
+    </div>
+  );
+};
+
+export default PageNotFound;
