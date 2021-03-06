f8106097c27fb83da6bd5e45ae9bfb65748998a5
Charles <47355793+MeatBoyed@users.noreply.github.com>
2021-03-07 01:45:02 +0200

    fix: Extracting of routes

--- Routes/productRoutes.js
+++ Routes/productRoutes.js
@@ -103,14 +103,16 @@ router.get('/search/:query', async (req, res, next) => {
 });
 
 router.get('/:id', async (req, res) => {
+  const productID = req.params.id;
+
   try {
-    const productID = req.params.id;
+    if (productID) res.status(400).json({ status: 400, message: 'Missing required field: productID' });
 
     const product = await Product.findById(productID);
 
     res.status(200).json(product);
   } catch (error) {
-    res.status(400).json({ msg: error.message });
+    res.status(400).json('Internal error');
   }
 });
 
