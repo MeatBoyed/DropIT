3b1a64ef527e7becd289ed1e338ec09f76eb251d
Charles <47355793+MeatBoyed@users.noreply.github.com>
2021-03-07 22:55:14 +0200

    feat: Filtering added to Search route

--- Routes/search.js
+++ Routes/search.js
@@ -8,58 +8,93 @@ const limit = 5;
 // Search Route
 router.get('/:query', async (req, res, next) => {
   const query = req.params.query;
+  const operation = req.query.operation;
   const page = req.query.page;
 
-  console.log(typeof page);
-
   try {
-    if (!query || !page || page == '0')
-      res.status(400).json({ status: 400, message: 'Missing required field: Page or Page == 0' });
+    if (!query || !operation || !page || page == '0')
+      return res.status(400).json({ status: 400, message: 'Missing required fields: Operation || Page || Page == 0' });
 
     const offset = limit * (page - 1);
+    let products;
 
-    const products = await Product.aggregate([
-      {
-        $search: {
-          index: 'homePageSearch',
-          text: {
-            query: query,
-            path: ['title', 'vendor', 'description', 'category'],
-            fuzzy: {
-              maxEdits: 2,
-              prefixLength: 3,
+    // Check operation type. 0 == Full text search, 1 == Filter
+    if (operation == '0') {
+      products = await Product.aggregate([
+        {
+          $search: {
+            index: 'homePageSearch',
+            text: {
+              query: query,
+              path: ['title', 'vendor', 'description', 'category'],
+              fuzzy: {
+                maxEdits: 2,
+                prefixLength: 3,
+              },
             },
           },
         },
-      },
-      {
-        $skip: offset,
-      },
-      {
-        $limit: limit,
-      },
-      {
-        $project: {
-          _id: 1,
-          title: 1,
-          price: 1,
-          vendor: 1,
-          'thumbnails.mainThumbnail': 1,
-          category: 1,
+        {
+          $skip: offset,
         },
-      },
-      {
-        $sort: {
-          price: -1,
-          rating: -1,
+        {
+          $limit: limit,
         },
-      },
-    ]);
+        {
+          $project: {
+            _id: 1,
+            title: 1,
+            price: 1,
+            vendor: 1,
+            'thumbnails.mainThumbnail': 1,
+            category: 1,
+          },
+        },
+        {
+          $sort: {
+            price: -1,
+            rating: -1,
+          },
+        },
+      ]);
+    } else if (operation == '1') {
+      products = await Product.aggregate([
+        {
+          $match: {
+            category: query,
+          },
+        },
+        {
+          $skip: offset,
+        },
+        {
+          $limit: limit,
+        },
+        {
+          $project: {
+            _id: 1,
+            title: 1,
+            price: 1,
+            vendor: 1,
+            'thumbnails.mainThumbnail': 1,
+            category: 1,
+          },
+        },
+        {
+          $sort: {
+            price: -1,
+            rating: -1,
+          },
+        },
+      ]);
+    }
 
     res.status(200).json(products);
   } catch (error) {
     res.status(400).json('Internal error');
   }
 });
+
+router.get('/:');
 
 module.exports = router;
