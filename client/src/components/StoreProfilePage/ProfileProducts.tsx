1677fb8d190b2f2a226a537b3d9f313918b60134
Charles <47355793+MeatBoyed@users.noreply.github.com>
2021-03-10 02:10:06 +0200

    feat: Pagination in profile

--- client/src/components/StoreProfilePage/ProfileProducts.tsx
+++ client/src/components/StoreProfilePage/ProfileProducts.tsx
@@ -1,6 +1,8 @@
-import React, { useState } from 'react';
+import React, { useCallback, useRef, useState } from 'react';
+import { Link } from 'react-router-dom';
 
 import { ItemCard } from '../ItemCard';
+import { LoadingSpinner } from '../LoadingSpinner';
 import { useFetchProfileProducts } from '../Utils/useFetchProfileProducts';
 
 interface Props {
@@ -9,25 +11,63 @@ interface Props {
 
 export const ProfileProducts: React.FC<Props> = ({ profileTitle }) => {
   const [pageNumber, setPageNumber] = useState<number>(1);
-  const { loading, error, products } = useFetchProfileProducts(profileTitle, pageNumber);
+  const { loading, error, products, hasMore } = useFetchProfileProducts(profileTitle, pageNumber);
+
+  const observer = useRef<IntersectionObserver>();
+  const lastItemElementRef = useCallback(
+    // Node relates to the last element that is rendered (last document)
+    (node) => {
+      // Stop scrolling if loading (fetching data)
+      if (loading) return;
+
+      // Connect observer to correct place
+      if (observer.current) observer.current.disconnect();
+      observer.current = new IntersectionObserver((entries) => {
+        if (entries[0].isIntersecting && hasMore) {
+          setPageNumber((previousPageNumber) => previousPageNumber + 1);
+        }
+      });
+      if (node) observer.current.observe(node);
+    },
+    [loading, hasMore]
+  );
 
   return (
-    <div id="ShoppingSection">
-      <p className="productsTitle">Products</p>
+    <section id="ShoppingSection">
       <div className="itemsContainer">
         {products.map((product, index) => {
-          return (
-            <ItemCard
-              key={index}
-              url={`/${product.vendor}/${product._id}`}
-              title={product.title}
-              price={product.price}
-              mainImage={product.thumbnails.mainThumbnail}
-            />
-          );
+          if (products.length === index + 1) {
+            return (
+              <div key={index} ref={lastItemElementRef} className="itemCard">
+                <Link to={`/${product.vendor}/${product._id}`}>
+                  <img src={product.thumbnails.mainThumbnail} alt="" className="itemImage" />
+                </Link>
+                <div className="itemInfo">
+                  <Link to={`/${product.vendor}/${product._id}`}>
+                    <p className="itemTitle">{product.title}</p>
+                  </Link>
+
+                  <Link to={`/${product.vendor}/${product._id}`}>
+                    <p className="itemPrice">{product.price}</p>
+                  </Link>
+                </div>
+              </div>
+            );
+          } else {
+            return (
+              <ItemCard
+                key={index}
+                url={`/${product.vendor}/${product._id}`}
+                title={product.title}
+                price={product.price}
+                mainImage={product.thumbnails.mainThumbnail}
+              />
+            );
+          }
         })}
-        {error.isError && <h1>{error.message}</h1>}
       </div>
-    </div>
+      <div className="loadingSpinner">{loading && <LoadingSpinner />}</div>
+      <div>{error.isError && <h3>{error.message}</h3>}</div>
+    </section>
   );
 };
