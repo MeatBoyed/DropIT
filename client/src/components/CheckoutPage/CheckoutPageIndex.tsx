46410f1c70ba3ee8a45014a924d13be8f14e078a
Charles <47355793+MeatBoyed@users.noreply.github.com>
2021-03-12 02:37:25 +0200

    feat: Main section JSX

--- client/src/components/CheckoutPage/CheckoutPageIndex.tsx
+++ client/src/components/CheckoutPage/CheckoutPageIndex.tsx
@@ -8,6 +8,13 @@ export const CheckoutPageIndex: React.FC = () => {
     <section id="CheckoutSection">
       <ShoppingCartSummary />
       <div id="PagesContainer">
+        <h2 className="sectionTitle">Grabbler</h2>
+        <div className="breadCrumsContaienr">
+          <p className="breadCrum">{'Cart >'}</p>
+          <p className="breadCrum">{'Information >'}</p>
+          <p className="breadCrum">{'Shipping >'}</p>
+          <p className="breadCrum">{'Payment'}</p>
+        </div>
         <Information />
       </div>
     </section>
