177e38f75e5bd3cd8ff094570b7a077b22686d23
Charles <47355793+MeatBoyed@users.noreply.github.com>
2021-03-12 03:31:02 +0200

    feat: Information styled

--- client/src/components/CheckoutPage/Information.tsx
+++ client/src/components/CheckoutPage/Information.tsx
@@ -6,32 +6,48 @@ export const Information: React.FC = () => {
       <form action="">
         <div className="contactContainer">
           <p className="formTitle">Contact Information</p>
-          <div className="inputContainer">
-            <input type="email" name="email" id="email" placeholder="Email" required className="formInput" />
+          <div className="inlineInputContainer">
+            <input type="email" name="email" id="email" placeholder="Email" required className="formInput firstInput" />
             <input type="tel" name="mobile" id="mobile" placeholder="Mobile phone" required className="formInput" />
           </div>
         </div>
         <div className="shippingContainer">
           <p className="formTitle">Shipping Address</p>
           <div className="shippingAddressContainer">
-            <div className="nameContainer">
-              <input type="text" name="firstname" id="firstname" placeholder="First Name" required className="formInput" />
+            <div className="inlineInputContainer">
+              <input
+                type="text"
+                name="firstname"
+                id="firstname"
+                placeholder="First Name"
+                required
+                className="formInput firstInput"
+              />
               <input type="text" name="lastname" id="lastname" placeholder="Last Name" required className="formInput" />
             </div>
-            <input type="text" name="address1" id="address" placeholder="Address" required className="formInput" />
-            <input
-              type="text"
-              name="address2"
-              id="address"
-              placeholder="Address line 2 (optional)"
-              required
-              className="formInput"
-            />
-            <select name="city" id="city" required>
-              <option value="Walvis Bay">Walvis Bay</option>
-              <option value="Swakopmund">Swakopmund</option>
-              <option value="Windhoek">Windhoek</option>
-            </select>
+            <div className="addressContainer">
+              <input
+                type="text"
+                name="address1"
+                id="address"
+                placeholder="Address"
+                required
+                className="formInput extended"
+              />
+              <input
+                type="text"
+                name="address2"
+                id="address"
+                placeholder="Address line 2 (optional)"
+                required
+                className="formInput extended"
+              />
+              <select name="city" id="city" required className="formSelectContainer formSelector">
+                <option value="Walvis Bay">Walvis Bay</option>
+                <option value="Swakopmund">Swakopmund</option>
+                <option value="Windhoek">Windhoek</option>
+              </select>
+            </div>
           </div>
         </div>
       </form>
