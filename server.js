e513d04c87a3a1efe705d1b341b89ae693584c1b
Charles <47355793+MeatBoyed@users.noreply.github.com>
2021-03-14 01:25:28 +0200

    feat: Production configs

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
+const path = require("path")
 
 const home = require('./Routes/home');
 const search = require('./Routes/search');
@@ -24,5 +25,16 @@ app.use('', home);
 app.use('/search', search);
 app.use('/product', product);
 app.use('/client-profile', clientProfile);
+
+// Serving of React static build files
+if (process.env.NODE_ENV === "production") {
+
+	// Set static folder
+	app.use(express.static*"client/build")
+
+	app.get("*", (req, res) => {
+		res.sendFile(path.resolve(__dirname, "client", "build", "index.html"))
+	})
+}
 
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
