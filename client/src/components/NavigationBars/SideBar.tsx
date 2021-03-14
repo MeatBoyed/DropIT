adcd2b23c96fe0ab729edbde03c6400c5f8fc02f
Charles <47355793+MeatBoyed@users.noreply.github.com>
2021-03-15 01:24:06 +0200

    fix: Removal of nonfunctional sidebar links

--- client/src/components/NavigationBars/SideBar.tsx
+++ client/src/components/NavigationBars/SideBar.tsx
@@ -16,26 +16,6 @@ const SideBar: React.FC = () => {
             Accessories
           </a>
         </li>
-        <li className="link">
-          <a href="/search?f=House+Appliance" className="tag">
-            House Appliances
-          </a>
-        </li>
-        <li className="link">
-          <a href="/search?f=Tech" className="tag">
-            Tech
-          </a>
-        </li>
-        <li className="link">
-          <a href="/search?f=Music" className="tag">
-            Music
-          </a>
-        </li>
-        <li className="link">
-          <Link className="tag" to="">
-            About
-          </Link>
-        </li>
       </ul>
     </section>
   );
