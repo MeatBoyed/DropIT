1a8a39a5d181a8f7561b5adf3de242a558f130b4
Charles <47355793+MeatBoyed@users.noreply.github.com>
2021-03-26 01:54:00 +0200

    fix: Save Payment Method function created

--- client/src/components/Utils/useCheckout.tsx
+++ client/src/components/Utils/useCheckout.tsx
@@ -1,23 +1,39 @@
 import { useEffect, useState } from 'react';
-import { UserInfo } from './Interfaces';
+import { UserData, UserInfo, PaymentMethod } from './Interfaces';
 
 export const useCheckout = () => {
-  const [userInfo, setUserInfo] = useState<UserInfo>();
+  const [userData, setUserData] = useState<UserData>({
+    userInfo: { email: '', phoneNumber: '', firstName: '', lastName: '', address1: '', address2: '', city: '' },
+    paymentMethod: 'none',
+  });
 
   useEffect(() => {
     let existing = localStorage.getItem('DROPTI');
 
-    if (existing != null) return setUserInfo(JSON.parse(existing));
+    if (existing != null) return setUserData(JSON.parse(existing));
   }, []);
 
   const SaveUserInfo = (payload: UserInfo) => {
     try {
-      localStorage.setItem('DROPTI', JSON.stringify(payload));
+      let newUserData: UserData = { userInfo: payload, paymentMethod: userData.paymentMethod };
+      setUserData(newUserData);
+      localStorage.setItem('DROPTI', JSON.stringify(newUserData));
       return true;
     } catch (error) {
       return false;
     }
   };
 
-  return { SaveUserInfo, userInfo };
+  const SavePaymentMethod = (payload: PaymentMethod) => {
+    try {
+      let newUserData: UserData = { userInfo: userData.userInfo, paymentMethod: payload };
+      setUserData(newUserData);
+      localStorage.setItem('DROPTI', JSON.stringify(newUserData));
+      return true;
+    } catch (error) {
+      return false;
+    }
+  };
+
+  return { SaveUserInfo, SavePaymentMethod, userData };
 };
