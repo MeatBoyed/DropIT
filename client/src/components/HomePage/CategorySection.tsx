de04516b57de6eb83b662291c5cc4fec71727883
Charles <47355793+MeatBoyed@users.noreply.github.com>
2021-04-04 22:30:47 +0200

    feat: Main Section styled and responsive with grid

---
+++ client/src/components/HomePage/CategorySection.tsx
@@ -0,0 +1,35 @@
+import React from 'react';
+
+import { ItemCard } from '../ItemCard';
+import { ProductCardModel } from '../Utils/Interfaces';
+
+interface Props {
+  headerText: string;
+  products: ProductCardModel[];
+}
+
+export const CategorySection: React.FC<Props> = ({ headerText, products }) => {
+  return (
+    <div className="categorySection">
+      <div className="header">
+        <p className="headerText">{headerText}</p>
+      </div>
+      <div className="productsContainer">
+        {products.map((product, index) => {
+          if (index < 3) {
+            return (
+              <ItemCard
+                key={index}
+                url={`/${product.vendor}/${product._id}`}
+                title={product.title}
+                price={product.price}
+                mainImage={product.thumbnails.mainThumbnail}
+              />
+            );
+          }
+        })}
+      </div>
+      <button className="viewMoreBtn">View More</button>
+    </div>
+  );
+};
