e9266d226b29804f8539d8f5bbc705a273f2f223
Charles <47355793+MeatBoyed@users.noreply.github.com>
2021-08-18 12:45:11 +0200

    feat: Filter options using Selector component

--- client/src/components/SearchPage/FilterBar.tsx
+++ client/src/components/SearchPage/FilterBar.tsx
@@ -1,20 +1,33 @@
 import React from 'react';
+import { useState } from 'react';
 
-import { ReactComponent as CheveronDown } from '../../images/chevronDown.svg';
+import { Selector } from '../Utils/Selector';
 
 interface Props {}
 
+let sortByOptions = ['Price', 'Product Name', 'Brand', 'Colour'];
+let orderByOptions = ['Asce', 'Desc'];
+
 export const FilterBar: React.FC<Props> = () => {
+  let [sortByOption, setSortByOption] = useState<string>('Product Name');
+  let [orderByOption, setOrderByOption] = useState<string>('Ascending');
+
   return (
     <div className="filterBar">
-      <div className="dropDown">
-        <p className="title">Sort By</p>
-        <CheveronDown />
-      </div>
-      <div className="dropDown">
-        <p className="title">Sort By</p>
-        <CheveronDown />
-      </div>
+      <p className="title">Sort By</p>
+      <Selector
+        NOMARGIN={sortByOption.length !== 0 ? '' : 'NOMARGIN'}
+        onChange={(newValue) => setSortByOption(newValue)}
+        title={''}
+        options={sortByOptions}
+      />
+      <p className="title">Order By</p>
+      <Selector
+        NOMARGIN={orderByOption.length !== 0 ? '' : 'NOMARGIN'}
+        onChange={(newValue) => setOrderByOption(newValue)}
+        title={''}
+        options={orderByOptions}
+      />
     </div>
   );
 };
