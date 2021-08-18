4219351179e3eb87d5eef9e9f201b929b368b961
Charles <47355793+MeatBoyed@users.noreply.github.com>
2021-08-18 19:51:07 +0200

    feat: Filter selectors styled

--- client/src/components/SearchPage/FilterBar.tsx
+++ client/src/components/SearchPage/FilterBar.tsx
@@ -6,28 +6,32 @@ import { Selector } from '../Utils/Selector';
 interface Props {}
 
 let sortByOptions = ['Price', 'Product Name', 'Brand', 'Colour'];
-let orderByOptions = ['Asce', 'Desc'];
+let orderByOptions = ['15', '30', '45'];
 
 export const FilterBar: React.FC<Props> = () => {
   let [sortByOption, setSortByOption] = useState<string>('Product Name');
-  let [orderByOption, setOrderByOption] = useState<string>('Ascending');
+  let [orderByOption, setOrderByOption] = useState<string>('15');
 
   return (
     <div className="filterBar">
-      <p className="title">Sort By</p>
-      <Selector
-        NOMARGIN={sortByOption.length !== 0 ? '' : 'NOMARGIN'}
-        onChange={(newValue) => setSortByOption(newValue)}
-        title={''}
-        options={sortByOptions}
-      />
-      <p className="title">Order By</p>
-      <Selector
-        NOMARGIN={orderByOption.length !== 0 ? '' : 'NOMARGIN'}
-        onChange={(newValue) => setOrderByOption(newValue)}
-        title={''}
-        options={orderByOptions}
-      />
+      <div className="selection">
+        <p className="title">Sort By</p>
+        <Selector
+          NOMARGIN={sortByOption.length !== 0 ? '' : 'NOMARGIN'}
+          onChange={(newValue) => setSortByOption(newValue)}
+          title={''}
+          options={sortByOptions}
+        />
+      </div>
+      <div className="selection">
+        <p className="title">Order By</p>
+        <Selector
+          NOMARGIN={orderByOption.length !== 0 ? '' : 'NOMARGIN'}
+          onChange={(newValue) => setOrderByOption(newValue)}
+          title={''}
+          options={orderByOptions}
+        />
+      </div>
     </div>
   );
 };
